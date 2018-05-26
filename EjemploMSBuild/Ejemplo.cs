f28c8652700892c9c176d6bc355d91fd531b9c67
Santiago A. Soriano Garcia <ssg127@inlumine.ual.es>
2018-05-26 18:04:24 +0200

    Agregar archivos de proyecto.

---
+++ EjemploMSBuild/Ejemplo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploMSBuild
+{
+    public class Ejemplo
+    {
+        private int x, y;
+
+        public Ejemplo(int a, int b)
+        {
+            this.x = a;
+            this.y = b;
+        }
+
+        public bool metodo(int m, int n)
+        {
+            if (m == n) return true;
+            return false;
+        }
+    }
+}
---
+++ TestEjemploMSBuild/TestEjemplo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EjemploMSBuild;
+
+namespace TestEjemploMSBuild
+{
+    [TestClass]
+    public class TestEjemplo
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x = 1;
+            int y = 1;
+
+            Ejemplo aux = new Ejemplo(x, y);
+            bool comprobar = aux.metodo(x, y);
+            Assert.IsTrue(comprobar);
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int x = 1;
+            int y = 2;
+
+            Ejemplo aux = new Ejemplo(x, y);
+            bool comprobar = aux.metodo(x, y);
+            Assert.IsFalse(comprobar);
+
+        }
+    }
+}
